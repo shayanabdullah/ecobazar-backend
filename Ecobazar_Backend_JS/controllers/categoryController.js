58954aa2cf04e8f5fbe6d7bebc96ec8b489eff03
shayanabdullah <tuhinafroza07@gmail.com>
2026-09-18 00:02:26 +0600

    update compiled JavaScript

--- Ecobazar_Backend_JS/controllers/categoryController.js
+++ Ecobazar_Backend_JS/controllers/categoryController.js
@@ -1,6 +1,6 @@
 import categoryModel from "../models/categoryModel.js";
 import uploadToCloudinary from "../utils/cloudinaryUpload.js";
-const categoryController = async (req, res) => {
+const categoryCreateController = async (req, res) => {
     try {
         const { categoryName, slug, description, status } = req.body;
         const img = req.file;
@@ -63,5 +63,52 @@ const categoryController = async (req, res) => {
         });
     }
 };
-export { categoryController };
+const updateCategoryController = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const categoryExist = await categoryModel.findById(id);
+        if (!categoryExist) {
+            return res.status(404).json({
+                success: false,
+                message: "Category was not found.",
+            });
+        }
+        const updatedCategory = await categoryModel.findByIdAndUpdate(id, req.body, { returnDocument: "after" });
+        return res.status(200).json({
+            success: true,
+            message: `${categoryExist.categoryName} category updated successfully.`,
+            data: updatedCategory,
+        });
+    }
+    catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error.",
+        });
+    }
+};
+const deleteCategoryController = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const categoryExist = await categoryModel.findById(id);
+        if (!categoryExist) {
+            return res.status(404).json({
+                success: false,
+                message: "Category was not found.",
+            });
+        }
+        await categoryModel.findByIdAndDelete(id);
+        return res.status(200).json({
+            success: true,
+            message: `${categoryExist.categoryName} category deleted successfully.`,
+        });
+    }
+    catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error.",
+        });
+    }
+};
+export { categoryCreateController, updateCategoryController, deleteCategoryController };
 //# sourceMappingURL=categoryController.js.map
